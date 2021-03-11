330992a650c1b158d600b9b3f4095889e4256b26
prateeksohlot <prateek.sohlot95@gmail.com>
2021-03-11 10:15:17 +0530

    speed limit added

--- speedlimit.py
+++ speedlimit.py
@@ -1,17 +1,30 @@
 import sys
 
-case1 = int(input())
-val = list()
+n = int(input())
+speed = []
+time = []
 
 for id,x in enumerate(sys.stdin):
     if id>=0:
         val = list(x.split())
-        print(val)
+        speed.append(int(val[0]))
+        time.append(int(val[1]))
         
-    if id== case1 -1:
+    if id== n-1:
         break
-        
 
+        
+dist = 0
+
+for i in range(len(time)):
+    if i >=1:
+        t = time[i] - time[i-1]
+        dist += speed[i] * t
+
+    else:
+        dist += speed[i] * time[i]
+
+print(dist,"miles")
 
 
 # 3
